f2a18473777b0c83085bb9a85d004472e4c13415
Sanek <sanek@live.com>
2024-06-25 20:59:31 +0300

    changed to val

--- src/main/kotlin/lesson_1/task_2.kt
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_1
 
 fun main() {
-    var quantityOfOrders: Int = 75
+    val quantityOfOrders: Int = 75
     var quantityOfEmployees: Int = 2000
     val notificationThanks: String = "Благодарим Вас за заказ"
     println("Кол-во заказов $quantityOfOrders, благодарность $notificationThanks")
