becb025a2ec13dec42708d2b72f8f24bfbe643c1
Sanek <sanek@live.com>
2024-06-25 21:17:10 +0300

    changed to non-U

--- src/main/kotlin/lesson_1/task_5.kt
+++ src/main/kotlin/lesson_1/task_5.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_1
 
 fun main() {
-    val totalSeconds: UShort = 6480U
-    val seconds = (totalSeconds % 60U).toString().padStart(2, '0')
-    val fullMinutes = totalSeconds / 60U
-    val minutes = (fullMinutes % 60U).toString().padStart(2, '0')
-    val hours = (fullMinutes / 60U).toString().padStart(2, '0')
+    val totalSeconds: Short = 6480
+    val seconds = totalSeconds % 60
+    val fullMinutes = totalSeconds / 60
+    val minutes = fullMinutes % 60
+    val hours = fullMinutes / 60
 
-    println("Гагарин провел в космосе $hours:$minutes:$seconds")
+    println("Гагарин провел в космосе ${String.format("%02d", hours)}:${String.format("%02d", minutes)}:${String.format("%02d", seconds)}")
 }
