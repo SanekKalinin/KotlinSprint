e3308744430921917640ccfd0bbfce155cab4124
Sanek <sanek@live.com>
2024-06-25 23:04:19 +0300

    initial

--- src/main/kotlin/lesson_2/task_3.kt
+++ src/main/kotlin/lesson_2/task_3.kt
@@ -1,5 +1,22 @@
 package org.example.lesson_2
 
 fun main() {
+    val hourDeparture = 9
+    val minuteDeparture = 39
+    val travelTotalMinutes = 457
+    val travelHours = hourDeparture + travelTotalMinutes/60
+    val travelMinutes = travelTotalMinutes%60
+    var hourArrival:Int = 0
+    var minuteArrival:Int = 0
 
+    If ( (minuteDeparture + travelMinutes) >= 60) {
+        hourArrival = travelHours + 1
+        minuteArrival = (minuteDeparture +  travelMinutes) - 60
+    }
+    else {
+        hourArrival = travelHours
+        minuteArrival = minuteDeparture +  travelMinutes
+    }
+
+    println("Время прибытия ${"%02d".format(hourArrival)} : ${"%02d".format(minuteArrival)} ")
 }
