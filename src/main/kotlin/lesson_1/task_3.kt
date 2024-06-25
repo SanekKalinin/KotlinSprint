ef3ffe769cb802c49830cef8a91817be45cc00a5
Sanek <sanek@live.com>
2024-06-25 21:02:20 +0300

    changed to string

--- src/main/kotlin/lesson_1/task_3.kt
+++ src/main/kotlin/lesson_1/task_3.kt
@@ -1,15 +1,13 @@
 package org.example.lesson_1
 
 fun main() {
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
     println("Год полета: $year")
-    //нужно по идее выводить String.format("%02d", hour) на случай ноль впереди, но в задании не указано
-    println("Вылет: 0$hour часов")
-    // либо minute.toString().padStart(2, '0')
-    println("0$minute минут")
-    hour = 10
-    minute = 55
+    println("Вылет: $hour часов")
+    println("$minute минут")
+    hour = "10"
+    minute = "55"
     println("Посадка в $hour:$minute")
 }
