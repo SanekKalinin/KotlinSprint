11d446110730c3f8ec595bff8254863c191f6b7f
SanekKalinin <44876749+SanekKalinin@users.noreply.github.com>
2024-06-26 22:09:42 +0300

    initial (#6)
    Co-authored-by: Sanek <sanek@live.com>

--- src/main/kotlin/lesson_2/task_1.kt
+++ src/main/kotlin/lesson_2/task_1.kt
@@ -1,5 +1,11 @@
 package org.example.lesson_2
 
 fun main() {
+    val score1 = 3
+    val score2 = 4
+    val score3 = 3
+    val score4 = 5
+    val averageScore = (score1 + score2 + score3 + score4).toFloat() / 4
 
+    println("Средний бал по классу ${"%.2f".format(averageScore)}")
 }
