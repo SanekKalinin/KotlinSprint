e2c156a1f283c19e06f27894bb55cac357ed6e5d
Sanek <sanek@live.com>
2024-06-26 23:56:30 +0300

    L3_T1 initial

--- src/main/kotlin/lesson_2/task_4.kt
+++ src/main/kotlin/lesson_2/task_4.kt
@@ -1,5 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
+    val dayGreeting = "день, "
+    val eveningGreeting = "вечер, "
+    val userName = "мр. Симпсон"
+    var greeting: String
 
+    greeting = dayGreeting + userName
+    println("Добрый $greeting")
+
+    greeting = eveningGreeting + userName
+    println("Добрый $greeting")
 }
