99eb2b192a202f00836c5cd93f50476d322b7d6c
SanekKalinin <44876749+SanekKalinin@users.noreply.github.com>
2024-06-26 13:56:31 +0300

    L1_task4_initial (#4)
    * L1_task4_initial
    
    * changed to non-U
    
    ---------
    
    Co-authored-by: Sanek <sanek@live.com>

---
+++ src/main/kotlin/lesson_1/task_4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах $length")
+    println("возраст Гагарина на момент полета (целых лет) $age")
+    println("какую часть дня Гагарин провел в космосе $partOfDay")
+    println("количество секунд которое Гагарин провел в космосе $seconds")
+    println("какую часть года Гагарин провел в космосе $partOfYear")
+    println("апогей орбиты Гагарина в метрах $apogee")
+}
