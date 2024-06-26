a614e7f48a891add8c95bf316febfcafa47d091e
Sanek <sanek@live.com>
2024-06-26 22:24:59 +0300

    L2_T2 code formating

--- src/main/kotlin/lesson_2/task_2.kt
+++ src/main/kotlin/lesson_2/task_2.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
-val employeesQuantity = 50
+    val employeesQuantity = 50
     val employeesSalary = 30000
     val internsQuantity = 30
     val internsSalary = 20000
-    val employeesPayroll = employeesQuantity*employeesSalary
-    val totalPayroll = employeesPayroll + internsQuantity*internsSalary
-    val averageSalary = totalPayroll/(employeesQuantity + internsQuantity)
+    val employeesPayroll = employeesQuantity * employeesSalary
+    val totalPayroll = employeesPayroll + internsQuantity * internsSalary
+    val averageSalary = totalPayroll / (employeesQuantity + internsQuantity)
+
     println("Расходы на выплату зарплаты постоянных сотрудников - $employeesPayroll")
     println("Общие расходы по ЗП после прихода стажеров - $totalPayroll")
     println("Среднюю ЗП одного сотрудника после устройства стажеров - $averageSalary")
