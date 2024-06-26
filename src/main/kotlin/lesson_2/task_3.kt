16c31cadfa2ec935823d2b953d6350a308da34b8
Sanek <sanek@live.com>
2024-06-26 22:46:05 +0300

    L2_T3 digits to consts and time without if

--- src/main/kotlin/lesson_2/task_3.kt
+++ src/main/kotlin/lesson_2/task_3.kt
@@ -1,22 +1,15 @@
 package org.example.lesson_2
 
+const val hourDeparture = 9
+const val minuteDeparture = 39
+const val travelTotalMinutes = 457
+
 fun main() {
-    val hourDeparture = 9
-    val minuteDeparture = 39
-    val travelTotalMinutes = 457
-    val travelHours = hourDeparture + travelTotalMinutes/60
-    val travelMinutes = travelTotalMinutes%60
-    var hourArrival:Int = 0
-    var minuteArrival:Int = 0
 
-    If ( (minuteDeparture + travelMinutes) >= 60) {
-        hourArrival = travelHours + 1
-        minuteArrival = (minuteDeparture +  travelMinutes) - 60
-    }
-    else {
-        hourArrival = travelHours
-        minuteArrival = minuteDeparture +  travelMinutes
-    }
+    val travelHours = hourDeparture + travelTotalMinutes / 60
+    val travelMinutes = travelTotalMinutes % 60
+    val hourArrival = travelHours + (minuteDeparture + travelMinutes) / 60
+    val minuteArrival = (minuteDeparture + travelMinutes) % 60
 
-    println("Время прибытия ${"%02d".format(hourArrival)} : ${"%02d".format(minuteArrival)} ")
+    println("Время прибытия ${"%02d:%02d".format(hourArrival, minuteArrival)}")
 }
