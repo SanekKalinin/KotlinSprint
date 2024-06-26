3a0e460b72523c0d7cde9dfbfffcbe5af6b0bff0
Sanek <sanek@live.com>
2024-06-26 22:57:43 +0300

    L2_T4 initial

--- src/main/kotlin/lesson_2/task_4.kt
+++ src/main/kotlin/lesson_2/task_4.kt
@@ -1,5 +1,12 @@
 package org.example.lesson_2
 
+const val crystalOre = 7
+const val ironOre = 11
+const val buff = 20
+
 fun main() {
+
+    println("Количество бонусной кристалической руды - ${buff * crystalOre / 100}")
+    println("Количество бонусной железной руды - ${buff * ironOre / 100}")
 
 }
