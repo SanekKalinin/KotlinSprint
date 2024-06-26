4c7e77a99af40e5f07c535c453b8fd6ce5d650a5
SanekKalinin <44876749+SanekKalinin@users.noreply.github.com>
2024-06-26 13:55:15 +0300

    L1_task2_initial (#2)
    * L1_task2_initial
    
    * changed to val
    
    ---------
    
    Co-authored-by: Sanek <sanek@live.com>

---
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+    val quantityOfOrders: Int = 75
+    var quantityOfEmployees: Int = 2000
+    val notificationThanks: String = "Благодарим Вас за заказ"
+    println("Кол-во заказов $quantityOfOrders, благодарность $notificationThanks")
+    //println("Кол-во работников: $quantityOfEmployees")
+    quantityOfEmployees = 1999
+    println("Кол-во работников: $quantityOfEmployees")
+}
