1893c51b5999c6a862d3c7b66dbe48890b47a4c6
Sanek <sanek@live.com>
2024-06-26 23:31:44 +0300

    L2_T5 initial

--- src/main/kotlin/lesson_2/task_5.kt
+++ src/main/kotlin/lesson_2/task_5.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_2
 
+import kotlin.math.pow
+
+const val deposit = 70_000
+const val percentage = 16.7
+const val period = 20
+
 fun main() {
+
+    val totalSum = deposit * (1 + percentage / 100).pow(period)
+
+    println("Размер вклада через 20 лет ${"%.3f".format(totalSum)}")
 
 }
