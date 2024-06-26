32c188ee08482d40d2f7f305a90fd544b1e57ee6
Sanek <sanek@live.com>
2024-06-27 00:06:20 +0300

    L3_T2 initial

--- src/main/kotlin/lesson_3/task_2.kt
+++ src/main/kotlin/lesson_3/task_2.kt
@@ -1,5 +1,11 @@
 package org.example.lesson_3
 
 fun main() {
+    val initialName = "Андреева Татьяна Сергеевна"
+    val requestDate = 20
+    val changedName = "Сидорова Татьяна Сергеевна"
+    val changedAge = 22
 
+    println("ФИО $initialName, возраст $requestDate")
+    println("ФИО $changedName, возраст $changedAge")
 }
