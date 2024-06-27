9ae4bc6f5be2c06cd869c4fb9ad1336e293efd57
Sanek <sanek@live.com>
2024-06-27 20:55:21 +0300

    L3_T2 const changes

--- src/main/kotlin/lesson_2/task_3.kt
+++ src/main/kotlin/lesson_2/task_3.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_2
 
-const val hourDeparture = 9
-const val minuteDeparture = 39
-const val travelTotalMinutes = 457
+const val MINUTES_IN_HOURS = 60
 
 fun main() {
-
-    val travelHours = hourDeparture + travelTotalMinutes / 60
-    val travelMinutes = travelTotalMinutes % 60
-    val hourArrival = travelHours + (minuteDeparture + travelMinutes) / 60
-    val minuteArrival = (minuteDeparture + travelMinutes) % 60
+    val hourDeparture = 9
+    val minuteDeparture = 39
+    val travelTotalMinutes = 457
+    val travelHours = hourDeparture + travelTotalMinutes / MINUTES_IN_HOURS
+    val travelMinutes = travelTotalMinutes % MINUTES_IN_HOURS
+    val hourArrival = travelHours + (minuteDeparture + travelMinutes) / MINUTES_IN_HOURS
+    val minuteArrival = (minuteDeparture + travelMinutes) % MINUTES_IN_HOURS
 
     println("Время прибытия ${"%02d:%02d".format(hourArrival, minuteArrival)}")
 }
