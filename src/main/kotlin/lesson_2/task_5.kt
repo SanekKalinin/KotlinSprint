5cbf1ef0b8eacd75937e9b17c8e1802ff96d0679
Sanek <sanek@live.com>
2024-06-27 21:02:41 +0300

    L2_T5 const changes

--- src/main/kotlin/lesson_2/task_5.kt
+++ src/main/kotlin/lesson_2/task_5.kt
@@ -2,12 +2,11 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-const val deposit = 70_000
-const val percentage = 16.7
-const val period = 20
-
 fun main() {
 
+    val deposit = 70_000
+    val percentage = 16.7
+    val period = 20
     val totalSum = deposit * (1 + percentage / 100).pow(period)
 
     println("Размер вклада через 20 лет ${"%.3f".format(totalSum)}")
