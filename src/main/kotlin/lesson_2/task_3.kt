0d0bd2fa4ee4e13cbc6d31cc3d0d708228f737e4
SanekKalinin <44876749+SanekKalinin@users.noreply.github.com>
2024-06-28 15:06:50 +0300

    Lesson_3_task_3 (#8)
    * initial
    
    * L2_T3 digits to consts and time without if
    
    * L3_T2 const changes
    
    ---------
    
    Co-authored-by: Sanek <sanek@live.com>

--- src/main/kotlin/lesson_2/task_3.kt
+++ src/main/kotlin/lesson_2/task_3.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_2
 
-fun main() {
+const val MINUTES_IN_HOURS = 60
 
+fun main() {
+    val hourDeparture = 9
+    val minuteDeparture = 39
+    val travelTotalMinutes = 457
+    val travelHours = hourDeparture + travelTotalMinutes / MINUTES_IN_HOURS
+    val travelMinutes = travelTotalMinutes % MINUTES_IN_HOURS
+    val hourArrival = travelHours + (minuteDeparture + travelMinutes) / MINUTES_IN_HOURS
+    val minuteArrival = (minuteDeparture + travelMinutes) % MINUTES_IN_HOURS
+
+    println("Время прибытия ${"%02d:%02d".format(hourArrival, minuteArrival)}")
 }
