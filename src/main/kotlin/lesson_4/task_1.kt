be51a95ed73e5719cbbbfb6121e9f0e8edeb7e4f
Sanek <sanek@live.com>
2024-06-28 16:11:53 +0300

    L4_T1 initial

--- src/main/kotlin/lesson_4/task_1.kt
+++ src/main/kotlin/lesson_4/task_1.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_4
 
-fun main() {
+const val TABLE_QUANTITY = 13
 
+fun main() {
+    val reservedForToday = 13
+    val reservedForTomorrow = 9
+
+    println("Доступность столиков на сегодня: ${reservedForToday < TABLE_QUANTITY},\n Доступность столиков на завтра: ${reservedForTomorrow < TABLE_QUANTITY}")
 }
