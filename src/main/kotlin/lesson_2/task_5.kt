53f2bded67baad27e216b7480e2f582d878a5168
SanekKalinin <44876749+SanekKalinin@users.noreply.github.com>
2024-06-28 15:07:51 +0300

    Lesson_2_task_5 (#10)
    * L2_T5 initial
    
    * L2_T5 const changes
    
    ---------
    
    Co-authored-by: Sanek <sanek@live.com>

--- src/main/kotlin/lesson_2/task_5.kt
+++ src/main/kotlin/lesson_2/task_5.kt
@@ -1,5 +1,14 @@
 package org.example.lesson_2
 
+import kotlin.math.pow
+
 fun main() {
+
+    val deposit = 70_000
+    val percentage = 16.7
+    val period = 20
+    val totalSum = deposit * (1 + percentage / 100).pow(period)
+
+    println("Размер вклада через 20 лет ${"%.3f".format(totalSum)}")
 
 }
