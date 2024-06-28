59e0ec615d09f800b17ee5fdace92809a5e5a589
Sanek <sanek@live.com>
2024-06-28 15:55:48 +0300

    L3_T5 initial

--- src/main/kotlin/lesson_3/task_5.kt
+++ src/main/kotlin/lesson_3/task_5.kt
@@ -1,5 +1,12 @@
 package org.example.lesson_3
 
 fun main() {
+    val inputData = "D2-D4;0"
+    val startPosition = inputData.split("-")[0]
+    val endPosition = inputData.split("-")[1].split(";")[0]
+    val moveNumber = inputData.split(";")[1]
 
+    println("Откуда $startPosition")
+    println("Куда $endPosition")
+    println("Номер хода $moveNumber")
 }
