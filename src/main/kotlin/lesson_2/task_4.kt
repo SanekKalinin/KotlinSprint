c701eb33c30702e1586329176419c81032225f66
SanekKalinin <44876749+SanekKalinin@users.noreply.github.com>
2024-06-28 15:07:13 +0300

    Lesson_2_task_4 (#9)
    * L2_T4 initial
    
    * L3_T2 const changes
    
    ---------
    
    Co-authored-by: Sanek <sanek@live.com>

--- src/main/kotlin/lesson_2/task_4.kt
+++ src/main/kotlin/lesson_2/task_4.kt
@@ -1,5 +1,11 @@
 package org.example.lesson_2
 
-fun main() {
+const val BUFF = 20
 
+fun main() {
+    val crystalOre = 7
+    val ironOre = 11
+
+    println("Количество бонусной кристалической руды - ${BUFF * crystalOre / 100}")
+    println("Количество бонусной железной руды - ${BUFF * ironOre / 100}")
 }
